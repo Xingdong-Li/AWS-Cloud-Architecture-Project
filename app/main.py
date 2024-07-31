8b54c3a1a07bef57b42d30345a6cc65dd7817c13
TaylorDavis16 <sheldonlee016@gmail.com>
2024-07-30 23:07:32 -0400

    modify github workflow

--- app/main.py
+++ app/main.py
@@ -12,6 +12,10 @@ s3 = boto3.client('s3', region_name='us-east-1', endpoint_url='http://localstack
 table = dynamodb.Table('Items')
 bucket_name = 'mybucket'
 
+@app.route('/health', methods=['GET'])
+def health_check():
+    return {"status": "healthy"}, 200
+
 @app.route("/items/<item_id>", methods=['GET'])
 def read_item(item_id):
     try:
